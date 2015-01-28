5f65d2d157db9b9b80a7f048750f59d513f180c6
George Navas <gannavas@gmail.com>
2015-01-28 10:45:28 -0800

    made a couple edits

--- ex1 - stripped table/stripped.js
+++ ex1 - stripped table/stripped.js
@@ -2,6 +2,7 @@ $( document ).ready( function(){
 
 $("form").submit(function(e){
 $("tbody tr:even").css("background",$("#color").val());
+$("input[type=text]").val("");
 	e.preventDefault();
 });
 
